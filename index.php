a5825a47653d29f4296c1b5a8991fc757e14caf3
Nehuensito <nehuenchibiurra@gmail.com>
2025-06-11 09:43:33 -0300

    Primer commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <h1>Portal Deportivo</h1>
+    <p>Tu lugar para comprar todo lo necesario para deportes al aire libre</p>
+  </header>
+  <main>
+    <a class="btn" href="login.html">Iniciar sesión</a>
+    <a class="btn" href="registro.html">Registrarse</a>
+  </main>
+</body>
+</html>
