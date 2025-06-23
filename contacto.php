812de2c1c59d37a829c7d320800cfbe6462d17f8
emilyluana20 <emilyluanna112@gmail.com>
2025-06-23 14:20:51 -0300

    actualizacion

--- contacto.php
+++ contacto.php
@@ -3,34 +3,70 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Contacto | Destiny </title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header class="Encabezado">
         <img src="C:\Users\emily\Downloads/destiny.jpg" alt="">
         <nav class="menu">
             <br><br>
-            <a href="vuelos.php" class="styled-link">Vuelos</a>
+            <a href="pages/" class="styled-link">Vuelos</a>
             <a href="pages/" class="styled-link">Paquetes</a>
-            <a href="hospedaje.php"class="styled-link">Hospedaje</a>
-            <a href="autos.php" class="styled-link">Autos</a>
-            <a href="contacto.php" class="styled-link">Contactos</a>
+            <a href="pages/"class="styled-link">Hospedaje</a>
+            <a href="pages/" class="styled-link">Autos</a>
+            <a href="pages/" class="styled-link">Contactos</a>
             <a href="#" class="carrito"><i class="fa-solid fa-cart-shopping"></i></a>
-            <button class="boton"><a href="login.php">Ingresar</a></button>
+            <button class="boton"><a href="pages/">Ingresar</a></button>
         </nav>
     </header>
+
+
+    <br><br><br>
+
+    <h1>¿Tenés dudas, ideas o sugerencias? <br>
+        Estamos para vos
+    </h1>
+
+    <br><br>
+
     <form action="">
-        <input type="text"><br>
-        <input type="text">
-        <input type="text">
-        <button type="button"></button>
+        <input type="text" name="Nombre" placeholder="Nombre">
+        <input type="text" name="Correo" placeholder="Correo">
+        <textarea name="Mensaje" placeholder="Escriba aquí su mensaje"></textarea>
+        <input type="button" value="enviar" id="boton">
     </form>
-    <h2>preguntas frecuentes</h2>
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Title</h5>
-            <p class="card-text">Content</p>
-        </div>
+
+<br><br><br>
+
+    <footer class="destiny-footer">
+  <div class="footer-container">
+    <!-- Sección Redes Sociales -->
+    <div class="footer-section">
+      <h3 class="footer-title">Seguinos en nuestras redes</h3>
+      <div class="social-icons">
+        <a href="#" class="social-icon"><img src="img/face.png" alt="Facebook"></a>
+        <a href="#" class="social-icon"><img src="img/ig.png" alt="Instagram"></a>
+        <a href="#" class="social-icon"><img src="img/tw.png" alt="Twitter"></a>
+      </div>
     </div>
+
+    <!-- Sección Atención al Cliente -->
+    <div class="footer-section">
+      <h3 class="footer-title">Atención al cliente y ventas</h3>
+      <p class="footer-text">+54 113674 6789</p>
+      <p class="footer-text">Lunes a domingos y feriados de 9 a 22 hs</p>
+    </div>
+
+    <!-- Sección Enlaces Rápidos (Destiny) -->
+    <div class="footer-section">
+      <h3 class="footer-title">DESTINY</h3>
+      <ul class="footer-links">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Preguntas frecuentes</a></li>
+      </ul>
+    </div>
+
+</footer>
 </body>
 </html>
