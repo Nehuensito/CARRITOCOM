7c0f74240bc145c727bc077527f2ba9399cbeadd
emilyluana20 <emilyluanna112@gmail.com>
2025-06-26 21:30:30 -0300

    actualizaciones

--- contacto.php
+++ contacto.php
@@ -4,70 +4,74 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contacto | Destiny </title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
 
 <body>
-    <header class="Encabezado">
-        <img src="img/destiny.jpg" alt="">
+
+     <header class="Encabezado">
+        <div class="logo-container">
+            <img src="img/destiny.jpg">
+        </div>
         <nav class="menu">
-            <br><br>
-            <a href="pages/" class="styled-link">Vuelos</a>
-            <a href="pages/" class="styled-link">Paquetes</a>
-            <a href="pages/"class="styled-link">Hospedaje</a>
-            <a href="pages/" class="styled-link">Autos</a>
-            <a href="pages/" class="styled-link">Contactos</a>
-            <a href="#" class="carrito"><i class="fa-solid fa-cart-shopping"></i></a>
-            <button class="boton"><a href="pages/">Ingresar</a></button>
+            <a href="" class="styled-link">Inicio</a>
+            <a href="" class="styled-link">Búsqueda</a>
+            <a href="" class="styled-link">Feed</a>
+            <a href="" class="styled-link">Reencuentros</a>
+            <a href="" class="styled-link">Contacto</a>
+            <a href="#" class="carrito"><ion-icon name="cart"></ion-icon></a>
+            <button class="boton"><a href="">Iniciar sesión</a></button>
         </nav>
-    </header>
+      </header>
+<hr>
 
+    <br><br><br><br>
 
-    <br><br><br>
-
-    <h1>¿Tenés dudas, ideas o sugerencias? <br>
-        Estamos para vos
+    <h1>
+      ¿Tenés dudas, ideas o sugerencias? <br>
+      Estamos para vos
     </h1>
 
-    <br><br>
-
     <form action="">
-        <input type="text" name="Nombre" placeholder="Nombre">
-        <input type="text" name="Correo" placeholder="Correo">
-        <textarea name="Mensaje" placeholder="Escriba aquí su mensaje"></textarea>
-        <input type="button" value="enviar" id="boton">
+        <h2>¡Contactanos!</h2>
+        <input type="text" name="Nombre" placeholder="Nombre...">
+        <input type="text" name="Correo" placeholder="Correo...">
+        <textarea name="Mensaje" placeholder="Escriba aquí su mensaje..."></textarea>
+        <input type="button" value="Enviar" id="boton">
     </form>
 
-<br><br><br>
+<br><br><br><br>
 
     <footer class="destiny-footer">
   <div class="footer-container">
-    <!-- Sección Redes Sociales -->
+
     <div class="footer-section">
       <h3 class="footer-title">Seguinos en nuestras redes</h3>
       <div class="social-icons">
-        <a href="#" class="social-icon"><img src="C:\Users\emily\OneDrive\Escritorio\TRASH SQUASH\face.png" alt="Facebook"></a>
-        <a href="#" class="social-icon"><img src="C:\Users\emily\OneDrive\Escritorio\TRASH SQUASH\ig.png" alt="Instagram"></a>
-        <a href="#" class="social-icon"><img src="C:\Users\emily\Downloads\tw.png" alt="Twitter"></a>
+        <a href="#" class="social-icon"><ion-icon name="logo-facebook"></ion-icon></a>
+        <a href="#" class="social-icon"><ion-icon name="logo-instagram"></ion-icon></a>
+        <a href="#" class="social-icon"><ion-icon name="logo-twitter"></ion-icon></a>
       </div>
     </div>
 
-    <!-- Sección Atención al Cliente -->
     <div class="footer-section">
       <h3 class="footer-title">Atención al cliente y ventas</h3>
       <p class="footer-text">+54 113674 6789</p>
       <p class="footer-text">Lunes a domingos y feriados de 9 a 22 hs</p>
     </div>
 
-    <!-- Sección Enlaces Rápidos (Destiny) -->
     <div class="footer-section">
       <h3 class="footer-title">DESTINY</h3>
       <ul class="footer-links">
-        <li><a href="#">Inicio</a></li>
+        <li><a href="index.php">Inicio</a></li>
         <li><a href="#">Preguntas frecuentes</a></li>
       </ul>
-    </div>
 
+    </div>
 </footer>
+
+<script type = "module" src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script> 
+<script nomodule src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js "></script>
+
 </body>
 </html>
